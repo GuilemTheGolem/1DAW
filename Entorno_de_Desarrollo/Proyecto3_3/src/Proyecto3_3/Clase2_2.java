fcdaf0f2a16dfbbce42d902589d8c5e21a67c732
GuilemTheGolem <epicguille3@gmail.com>
2024-10-25 15:48:09 +0200

    hola

---
+++ Entorno_de_Desarrollo/Proyecto3_3/src/Proyecto3_3/Clase2_2.java
@@ -0,0 +1,15 @@
+package Proyecto3_3;
+
+import java.util.Scanner;
+
+public class Clase2_2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Practica3_3 " + "Guillermo " );
+
+	}
+
+}
